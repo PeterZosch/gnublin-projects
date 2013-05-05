bac866afcc849509672e034dde56a0601d874dd7
Peter Zosh <tobias@theile.me>
2013-05-06 00:59:01 +0200

    lib hinzugügen, und ir-send

--- ir-send/main.c
+++ ir-send/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "gpio.h"
 
-#define HBL 888.888
+#define HBL 500000 
 
 void pulse();
 void sendHigh();
@@ -10,13 +11,21 @@ void sendLow();
 void setHigh();
 void setLow();
 
-int main(){
+int main() {
     
-unsigned int b = 0;
+	unsigned int b = 0;
     
-int ircode[16] = {1,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1};
+	int ircode[16] = {1,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1};
+
+	int n = 0;
     
-	for( int n = 0 ; n < 16 ; n++ ){
+	MakeGpio( Gpio11 );
+	DirectionGpio( Gpio11, GpioOut );
+	SetValueGpio( Gpio11, GpioClear );
+
+
+
+	for ( n = 0 ; n < 16 ; n++ ) {
         
 		b = ircode[n];
         
@@ -24,20 +33,22 @@ int ircode[16] = {1,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1};
             
 			sendHigh();
             
-		}else if(b == 0){
+		} else if (b == 0){
             
 			sendLow();
-	}	
-}
+		}	
+	}
 
 	printf("Fertig !");
-    
+
+	return 0;    
 }
 
 void pulse(){
   
-    
-	for( int i=0 ; i < 32 ; i++){
+   	int i = 0;
+ 
+	for( i=0 ; i < 32 ; i++){
 
 		setHigh();
 		usleep(27.78);
@@ -67,10 +78,13 @@ void sendLow(){
 }
 
 
-void setHigh(){
+void setHigh()
+{
+	SetValueGpio( Gpio11, GpioSet );
     
 }
 
-void setLow(){
-    
+void setLow()
+{
+	SetValueGpio( Gpio11, GpioClear );    
 }
---
+++ lib/clockout.h
@@ -0,0 +1,3 @@
+/** \file */
+
+extern int FrequOut(char gpio[3], unsigned long int Freq, int count);
