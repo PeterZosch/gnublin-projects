1f26bdc1105bb024e9c269f92e442a2acfc13b6f
Peter Zosch <tobias@theile.me>
2013-06-13 21:56:12 +0200

    struct in parser gebaut

--- irsend/src/parser.h
+++ irsend/src/parser.h
@@ -2,4 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct ircstruct *parse( struct ircstruct *ircode, char *argv[] );
+struct ircstruct{
+    unsigned int sb_pulse;
+    unsigned int sb_space;
+    unsigned int adress;
+    unsigned int command;
+    unsigned int stop;
+    };
+
+int parse( struct ircstruct *ircode, char *argv[] );
